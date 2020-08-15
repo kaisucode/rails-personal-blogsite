8135bf7d0280ea99d2915df074b1c59db8a98891
Kevin Hsu <kevin.hsukaihao@gmail.com>
2020-08-14 22:20:36 -0700

    In case I screw up

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,5 +1,15 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
+const chokidar = require('chokidar')
 
 module.exports = environment.toWebpackConfig()
+
+environment.config.devServer.before = (app, server) => {
+  chokidar.watch([
+    'config/locales/**/*.yml',
+    'app/views/**/*.html.erb', 
+    'app/assets/**/*.scss'
+  ]).on('change', () => server.sockWrite(server.sockets, 'content-changed'))
+}
+
